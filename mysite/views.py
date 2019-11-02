0b85afadc8a3636db4c19f25b8cd1bec0e14250a
Sajid Abdullah Mustafy <samustafy1994@gmail.com>
2019-11-02 11:52:05 +0600

    index

--- mysite/views.py
+++ mysite/views.py
@@ -1,20 +1,51 @@
 from django.shortcuts import render
 from .models import Contact
+import requests, json
 
 def index(request):
-    return render(request, 'mysite/index.html')
+    if request.method == 'POST':        
+        fname = request.POST.get('firstname')
+        lname = request.POST.get('lastname')
+        
+        r = requests.get("http://api.icndb.com/jokes/random?firstName=" + fname + "&lastName=" + lname)
+        json_data = json.loads(r.text)
+        joke = json_data.get('value').get('joke')
+        
+        context = {
+                "joker": joke
+                }
+        print(joke)
+        
+        return render(request, 'mysite/index.html', context)
+    else:
+        fname = "Sajid Abdullah"
+        lname = "Mustafy"
+        
+        r = requests.get("http://api.icndb.com/jokes/random?firstName=" + fname + "&lastName=" + lname)
+        json_data = json.loads(r.text)
+        joke = json_data.get('value').get('joke')
+        
+        context = {
+                "joker": joke
+                }
+        #print(joke)
+        
+        return render(request, 'mysite/index.html', context)    
+    
 
 def portfolio(request):
     return render(request, 'mysite/portfolio.html')
 
 def contact(request):
+    
     if request.method == 'POST':
         email_r = request.POST.get('email')
         subject_r= request.POST.get('subject')
         message_r = request.POST.get('message')
         
         c = Contact(email=email_r, subject=subject_r, message=message_r)
-        c.save()        
+        c.save()
+
         return render(request, 'mysite/thank.html')
     else:
         return render(request, 'mysite/contact.html')
